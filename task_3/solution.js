5f4655a6aaa802a7555e71fb52bc594e0691b6e2
psakharov <pavel@psakharov.ru>
2023-05-26 14:58:12 +0300

    Задание «Сложные структуры данных»

--- task_3/solution.js
+++ task_3/solution.js
@@ -23,6 +23,15 @@ function renderCartItem(item) {
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 
+    let itemCountText = '';
+    itemCountText = itemCountText + item.count;
+    itemCountText = itemCountText + ` × `;
+    itemCountText = itemCountText + item.price;
+    itemCountText = itemCountText + ` ₽ = `;
+    let sum = item.count * item.price;
+    itemCountText = itemCountText + sum;
+    itemCountText = itemCountText + ` ₽`;
+
     // Конец решения задания №3.1.
 
     return `<div class="cart-item"><div>${product.title}</div><div>${itemCountText}</div></div>`;
